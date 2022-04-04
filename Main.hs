e9191c2779a48413faf2d6af39e0e64090749769
Colin McDonald <colinmcd0731@gmail.com>
2022-04-03 20:47:48 -0400

    Minor changes

--- Main.hs
+++ Main.hs
@@ -6,12 +6,9 @@ import CutElim
 
 main :: IO ()
 main =
-  let --c = Conj (Atom "A") (Atom "B")
-      c = Imp (Conj (Neg (Atom "A")) (Imp (Imp (Atom "B") (Conj (Atom "A") (Atom "A"))) (Imp (Atom "C") (Atom "D")))) (Disj (Atom "C") (Neg (Atom "D")))
-      l = leaf (Disj (Atom "C") (Neg (Atom "D")))
---      r = leaf (Conj (Atom "A") (Atom "B"))
+  let c = Imp (Conj (Neg (Atom "A")) (Imp (Imp (Atom "B") (Disj (Atom "C") (Atom "A"))) (Imp (Atom "C") (Atom "D")))) (Conj (Disj (Atom "C") (Atom "A")) (Neg (Atom "D")))
+      l = leaf (Disj (Atom "C") (Atom "A"))
       l' = ensureValid l l
---      r' = ensureValid r r
       x = cut (weakeningR c l') (weakeningL c l')
       x' = ensureValid x x
       xcf = ensureValid x' (cutElim x')
